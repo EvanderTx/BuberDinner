743f1af027a40e40e78b05c2bd2869f1d39ade82
EvanderTx <evanderdiastx@gmail.com>
2026-01-29 07:48:27 +0100

    Parte 4 - Global Error Handling

---
+++ BuberDinner.Api/MiddleWare/ErrorHandlingMidleware.cs
@@ -0,0 +1,35 @@
+using System.Net;
+using System.Text.Json;
+
+namespace BuberDinner.Api.MiddleWare;
+
+public class ErrorHandlingMidleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorHandlingMidleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var code = HttpStatusCode.InternalServerError; // 500 if unexpected
+        var result = JsonSerializer.Serialize(new { error = "An error occurred while processing your request." });
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)code;
+        return context.Response.WriteAsync(result);
+    }
+}
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,6 +1,11 @@
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
 using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Api.MiddleWare;
+using BuberDinner.Api.Filters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using BuberDinner.Api.Errors;
+using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -8,11 +13,26 @@ var builder = WebApplication.CreateBuilder(args);
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
 
-    builder.Services.AddControllers();  
+    // builder.Services.AddControllers(options=>
+    // {
+    //     options.Filters.Add<ErrorHandlingFilterAttribute>();
+    // });
+    builder.Services.AddControllers();
+
+    //builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
+
 }
 
 var app = builder.Build();
 {
+    //app.UseMiddleware<ErrorHandlingMidleware>();
+    app.UseExceptionHandler("/error");
+    app.Map("/error", (HttpContext httpContext) =>
+    {
+        Exception? exception = httpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+        return Results.Problem();
+    });
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
